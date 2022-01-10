ae70f036166162e18e72a3f6ca4cc3cabcda6aec
Bereket Zergaw <arkishasoftware@gmail.com>
2022-01-10 12:55:33 +0300

    create-components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'/',
+    component:HomeComponent
+  },
+  {
+    path:'/login',
+    component:LoginComponent
+  },
+  {
+    path:'/register',
+    component:RegisterComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
